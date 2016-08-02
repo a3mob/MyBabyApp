32462dbd14e2cb78ec1bb2b890ae13967fa02a0e
gustavoetoa3mob <gustavo.eto@a3mob.com.br>
2016-08-02 15:32:48 -0300

    teste #3 Gustavo

--- app/src/main/java/br/com/a3mob/mybabyapp/MainActivity.java
+++ app/src/main/java/br/com/a3mob/mybabyapp/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //teste1 Will
         // teste #1 alteracao Gustavo
+        // teste #3 alteracao Gustavo
     }
 }
